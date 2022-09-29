81a234b291df8a14cc4709ee49cb026747ed67fa
DanielPC <danieleduardo.pc@outlook.com>
2022-09-28 23:13:37 -0500

    login

--- cielohamaca/src/app/data/login/login.component.ts
+++ cielohamaca/src/app/data/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  contactForm!: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.contactForm = this.initForm();
   }
 
+  onSubmit(): void {
+    console.log('Form ->')
+  }
+
+  initForm(): FormGroup{
+    return this.fb.group({
+      email: ['', [Validators.required, Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$')]],
+    })
+  }
 }
